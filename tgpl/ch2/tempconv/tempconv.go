2b9bcde829a8a5892d3095bf9ed0634b726c72bc
Toni Schmidbauer <toni@stderr.at>
2016-05-06 21:00:15 +0200

    import tempconv example

---
+++ tgpl/ch2/tempconv/tempconv.go
@@ -0,0 +1,18 @@
+package tempconv
+
+import "fmt"
+
+type Celcius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC = -273.15
+	FreezingC = 0
+	BoilingC = 100
+)
+
+func (c Celcius) String() string { return fmt.Sprintf("%g^C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g^C", f) }
+func (k Kelvin) String() string { return fmt.Sprintf("%g^C", k) }
+

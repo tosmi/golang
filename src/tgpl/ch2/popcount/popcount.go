97acedd8fc24978e93abb7b3702451d08384207e
Toni Schmidbauer <toni@stderr.at>
2016-05-31 18:23:39 +0200

    work on popcount

--- src/tgpl/ch2/popcount/popcount.go
+++ src/tgpl/ch2/popcount/popcount.go
@@ -1,13 +1,17 @@
 package popcount
 
-import 	"fmt"
+import 	(
+	"fmt"
+	"strconv"
+)
 
 var pc [256]byte
 
 func init() {
 	for i := range pc {
+		//fmt.Printf("before: i=%v : pc[%v]=%v : pc[%v/2]=%v : byte(i&1)=%v\n", i, i, pc[i], i, pc[i/2], byte(i&1)) 
 		pc[i] = pc[i/2] + byte(i&1)
-		fmt.Printf("%v: %v\n", i, pc[i])
+		fmt.Printf("after:  i=%v : pc[%v]=%v : pc[%v/2]=%v : byte(i&1)=%v %v\n", i,i , pc[i], i, pc[i/2], byte(i&1), strconv.FormatInt(int64(i),2))
 		
 	}
 }

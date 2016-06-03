a342753f6e2ab79f998ffce997d27b7291af8588
Toni Schmidbauer <toni@stderr.at>
2016-06-03 18:44:48 +0200

    worko popcount

--- src/tgpl/ch2/popcount/popcount.go
+++ src/tgpl/ch2/popcount/popcount.go
@@ -7,16 +7,21 @@ import 	(
 
 var pc [256]byte
 
+const debug = true
+
 func init() {
 	for i := range pc {
-		//fmt.Printf("before: i=%v : pc[%v]=%v : pc[%v/2]=%v : byte(i&1)=%v\n", i, i, pc[i], i, pc[i/2], byte(i&1)) 
 		pc[i] = pc[i/2] + byte(i&1)
-		fmt.Printf("after:  i=%v : pc[%v]=%v : pc[%v/2]=%v : byte(i&1)=%v %v\n", i,i , pc[i], i, pc[i/2], byte(i&1), strconv.FormatInt(int64(i),2))
-		
+		if debug {
+			fmt.Printf("after:  i=%3v : pc[%3v]=%v : pc[%3v/2]=%v : byte(i&1)=%v %v\n", i,i , pc[i], i, pc[i/2], byte(i&1), strconv.FormatInt(int64(i),2))
+		}
 	}
 }
 
 func Popcount(x uint64) int {
+	if debug {
+		fmt.Printf("x=%v : (x>>(1*8))=%v : byte(x>>(1*8))=%v\n",strconv.FormatInt(int64(x),2) ,strconv.FormatInt(int64(x>>(1*8)),2), byte(x>>(0*8)))
+	}
 	return int(pc[byte(x>>(0*8))] +
 		pc[byte(x>>(1*8))] +
 		pc[byte(x>>(2*8))] +

1ac749a83683cf56b818a128a2a59a2a12a7c2d6
Toni Schmidbauer <toni@stderr.at>
2016-11-08 15:20:15 +0100

    scheduling and ticking works

--- time/time.go
+++ time/time.go
@@ -7,54 +7,64 @@ import (
 )
 
 func doit() {
-
 	fmt.Println("do stuff")
-
-
-
 }
-
-func schedule() {
-	fmt.Println("in schedule")
-
-	doit()
-
-
-}
-
 
 
 // main
 func main()  {
-	const layout = "2006.01.02 15:04"
-	const start = "2016.11.04 07:50"
+	// const layout = "2006.01.02 15:04"
+	// const start = "2016.11.04 07:50"
+
+	const layout = "15:04"
+	const start = "15:09"
 	const interval = "2s"
 
 	loc, _ := time.LoadLocation("Europe/Vienna")
-
-	t,err := time.ParseInLocation(layout, start, loc); if err != nil {
+	t, err := time.ParseInLocation(layout, start, loc); if err != nil {
 		log.Fatal(err)
 	}
 
-	duration := time.ParseDuration(interval)
+	now := time.Now()
 
-	if t.Before(time.Now()) {
-		fmt.Println("Schedule start is before now!")
+	today := time.Date(now.Year(), now.Month(), now.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), loc )
+
+
+	if today.Before(now) {
+		fmt.Printf("Schedule %v is in past, moving to next day\n", today)
+		today = today.Add(time.Hour*24)
 	}
 
-	d := t.Sub(time.Now())
-	fmt.Println(d)
+	fmt.Printf("Waiting until %v\n", today)
 
-	time.AfterFunc(d, func() {
-		fmt.Println("expired")
-	})
+	waittime := today.Sub(now)
 
-	dc := make(chan time.Duration)
-	time.AfterFunc(d, schedule)
+	fmt.Printf("Waiting %v to start\n", waittime)
+
+	time.Sleep(waittime)
+
+	duration,_ := time.ParseDuration(interval)
+	ticker := time.NewTicker(duration)
+
+	for {
+		time := <- ticker.C
+		fmt.Printf("Running at %v\n", time)
+		doit()
+	}
 
 
-	fmt.Println(t)
-	fmt.Println(t.Sub(time.Now()))
+	// D := t.Sub(time.Now())
+	// fmt.Println(d)
 
-	time.Sleep(time.Hour)
+	// time.AfterFunc(d, func() {
+	// 	fmt.Println("expired")
+	// })
+
+	// dc := make(chan time.Duration)
+	// time.AfterFunc(d, schedule)
+
+
+	// fmt.Println(t.Sub(time.Now()))
+
+	// time.Sleep(time.Hour)
 }

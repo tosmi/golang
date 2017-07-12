ca376859561ff32700b8aac8adc53263b9c3eecf
Toni Schmidbauer <toni@stderr.at>
2017-07-12 08:21:14 +0200

    import ch6 trick

---
+++ src/tgpl/ch6/trick.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var cache = struct {
+	sync.Mutex
+	mapping map[string]string
+} {
+	mapping: make(map[string]string),
+}
+
+func Lookup(key string) string {
+	cache.Lock()
+	v := cache.mapping[key]
+	cache.Unlock()
+	return v
+}
+
+func main() {
+	cache.mapping["akey"] = "avalue"
+	fmt.Println("%T", cache.mapping)
+}
